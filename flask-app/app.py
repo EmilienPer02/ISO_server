ee786205342cae3a6a2de9acec55fbdb875093e9
EmilienPer02 <141314548+EmilienPer02@users.noreply.github.com>
2023-08-09 15:49:03 +0200

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,28 +4,24 @@ import hvac
 import time
  
 app = Flask(__name__)
-i=0
-while i<10:
-        try:
-                client = hvac.Client(url='http://vault:8200')
-                client.token = 'root-token'  # Use the root token for this example, not for production
-                token_response = client.create_token(policies=['default'], ttl='1h')
-                secret_response = client.secrets.transit.generate_data_key(
+client = hvac.Client(url='http://vault:8200')
+client.token = 'root-token'  # Use the root token for this example, not for production
+@app.route('/')
+def hello_world():
+    token_response = client.create_token(policies=['default'], ttl='1h')
+    # Create a token for the Flask app
+    flask_app_token = token_response['auth']['client_token']
+    print(flask_app_token)
+    # Retrieve the secret from Vault
+    try:
+        secret_response = client.secrets.kv.v2.read_secret_version(path='flask-secrets')
+    except:
+        secret_response = client.secrets.transit.generate_data_key(
                         name='my-key-name',
                         context={'app': 'flask-app'},
                         key_type='plaintext',
                     )
-                break;
-        except:
-                time.sleep(20)
-                i+=1
-print("=========",i )
-@app.route('/')
-def hello_world():
-    # Create a token for the Flask app
-    flask_app_token = token_response['auth']['client_token']
-    # Retrieve the secret from Vault
-    secret_response = client.secrets.kv.v2.read_secret_version(path='flask-secrets')
+         
     secret_value = secret_response['data']['data']['secret_value']
 
     return f'Welcome! Secret from Vault: {secret_value}'
