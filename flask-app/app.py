502ae8a80c27daa7420b8cfd286ea6e378572106
EmilienPer02 <141314548+EmilienPer02@users.noreply.github.com>
2023-08-09 14:31:33 +0200

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -7,19 +7,16 @@ app = Flask(__name__)
 # Initialize the Vault client
 client = hvac.Client(url='http://vault:8200')
 client.token = 'root-token'  # Use the root token for this example, not for production
+token_response = client.create_token(policies=['default'], ttl='1h')
+secret_response = client.secrets.transit.generate_data_key(
+        name='my-key-name',
+        context={'app': 'flask-app'},
+    )
 
 @app.route('/')
 def hello_world():
     # Create a token for the Flask app
-    token_response = client.create_token(policies=['default'], ttl='1h')
     flask_app_token = token_response['auth']['client_token']
-
-    # Store a secret in Vault
-    client.secrets.kv.v2.create_or_update_secret(
-        path='flask-secrets',
-        secret=dict(secret_value='my-secret')
-    )
-
     # Retrieve the secret from Vault
     secret_response = client.secrets.kv.v2.read_secret_version(path='flask-secrets')
     secret_value = secret_response['data']['data']['secret_value']
